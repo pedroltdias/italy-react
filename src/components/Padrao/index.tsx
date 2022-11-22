f885aa9b18992933d33cc6704c08c7b553cf194d
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-11-22 16:34:03 -0300

    esqueleto components

--- src/components/Padrao/index.tsx
+++ src/components/Padrao/index.tsx
@@ -1,7 +1,18 @@
+import styles from "./Padrao.module.scss";
+import { Outlet } from "react-router-dom";
+import stylesTema from "styles/Tema.module.scss";
+
 export default function Padrao() {
 	return (
-		<div>
-			Padrao
-		</div>
+		<>
+			<header className={styles.header}>
+				<div className={styles.header__text}>
+					A sua viagem, nossa prioridade.
+				</div>
+			</header>
+			<div className={stylesTema.container}>
+				<Outlet />
+			</div>
+		</>
 	);
-}
+} 
--- src/components/TagsRoteiro/index.tsx
+++ src/components/TagsRoteiro/index.tsx
@@ -1,10 +1,10 @@
 import classNames from "classnames";
+import { Roteiro } from "types/Roteiro";
 import styles from "./TagsRoteiro.module.scss";
 
 export default function TagsRoteiro({
 	category,
-	size,
-	serving,
+	duration,
 	price
 }: Roteiro) {
 	return (
@@ -13,8 +13,7 @@ export default function TagsRoteiro({
 				[styles.tags__tipo]: true,
 				[styles[`tags__tipo__${category.label.toLowerCase()}`]]: true,
 			})}>{category.label}</div>
-			<div className={styles.tags__porcao}>{size}g</div>
-			<div className={styles.tags__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? "" : "s"}</div>
+			<div className={styles.tags__qtdpessoas}>Serve {duration} dia{duration === 1 ? "" : "s"}</div>
 			<div className={styles.tags__valor}>R$ {price.toFixed(2)}</div>
 		</div>
 	);
