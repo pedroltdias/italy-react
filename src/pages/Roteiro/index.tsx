959e66ecd0be41270b89887a8a7be89115deb3a7
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-11-23 16:36:55 -0300

    pagina de roteiro

--- src/pages/Roteiro/index.tsx
+++ src/pages/Roteiro/index.tsx
@@ -1,7 +1,42 @@
+import styles from "./Roteiro.module.scss";
+import { Route, Routes, useNavigate, useParams } from "react-router-dom";
+import roteiros from "data/roteiros.json";
+import TagsRoteiro from "components/TagsRoteiro";
+import Inexistente from "pages/Inexistente";
+import Padrao from "components/Padrao";
+
 export default function Roteiro() {
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const roteiro = roteiros.find(item => item.id === Number(id));
+	if (!roteiro) {
+		return <Inexistente />;
+	}
 	return (
-		<div>
-			Roteiro
-		</div>
+		<Routes>
+			<Route path="*" element={<Padrao />}>
+				<Route index element={
+					<>
+						<button className={styles.voltar} onClick={() => navigate(-1)}>
+							{"< Voltar"}
+						</button>
+						<section className={styles.container}>
+							<h1 className={styles.titulo}>
+								{roteiro.title}
+							</h1>
+							<div className={styles.imagem}>
+								<img src={roteiro.photo} alt={roteiro.title} />
+							</div>
+							<div className={styles.conteudo}>
+								<p className={styles.conteudo__descricao}>
+									{roteiro.description}
+								</p>
+								<TagsRoteiro {...roteiro} />
+							</div>
+						</section>
+					</>
+				} />
+			</Route>
+		</Routes>
 	);
 }
