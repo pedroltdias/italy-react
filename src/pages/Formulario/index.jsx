85c0f0e70d91311605d1a90ff3cfdfc3fe47cdec
Diogo0602x <diogoteixeira0602@gmail.com>
2022-12-13 19:06:27 -0300

    refactor form

--- src/pages/Formulario/index.jsx
+++ src/pages/Formulario/index.jsx
@@ -11,12 +11,64 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-export default function Formulario() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-  function refreshPage(){ 
-    alert("Formulário enviado com sucesso!");
-    window.location.reload(); 
-  }
+export default function Formulario() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [nascimento, setNascimento] = useState('');
+  const [naturalidade, setNaturalidade] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [celular, setCelular] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [endereco, setEndereco] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+    if (nome && email && nascimento && naturalidade && telefone && celular && endereco && cidade) {
+        axios.post('http://localhost:3000/clientes', {
+          nome,
+          email,
+          nascimento,
+          naturalidade,
+          telefone,
+          celular,
+          endereco,
+          cidade
+        })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  
+    useEffect(() => {
+      axios.get('http://localhost:3000/clientes')
+        .then((response) => {
+          const { nome, email, nascimento, naturalidade, telefone, celular, endereco, cidade } = response.data;
+          setNome(nome);
+          setEmail(email);
+          setNascimento(nascimento);
+          setNaturalidade(naturalidade);
+          setTelefone(telefone);
+          setEndereco(endereco)
+          setCelular(celular);
+          setCidade(cidade);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []); 
+
+    function refreshPage(){ 
+      alert("Formulário enviado com sucesso!");
+      window.location.reload(); 
+    }
 
   return (
     <Box h="100vh">
@@ -47,45 +99,45 @@ export default function Formulario() {
             p="6"
             boxShadow="0 1px 2px #ccc"
           >
-            <FormControl display="flex" flexDir="column" gap="4">
+            <form display="flex" flexDir="column" gap="4" onSubmit={handleSubmit}>
               <HStack spacing="4">
                 <Box w="100%">
                   <FormLabel htmlFor="nome">Nome Completo</FormLabel>
-                  <Input id="nome" />
+                  <Input id="nome" value={nome} onChange={e => setNome(e.target.value)}/>
                 </Box>
                 <Box w="100%">
                   <FormLabel htmlFor="email">E-mail</FormLabel>
-                  <Input id="email" type="email" />
+                  <Input id="email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
                 </Box>
               </HStack>
               <HStack spacing="4">
                 <Box w="100%">
                   <FormLabel htmlFor="nasc">Data de Nascimento</FormLabel>
-                  <Input id="nasc" type="date" />
+                  <Input id="nascimento" type="date" value={nascimento} onChange={e => setNascimento(e.target.value)} />
                 </Box>
                 <Box w="100%">
                   <FormLabel htmlFor="natural">Naturalidade</FormLabel>
-                  <Input id="natural" />
+                  <Input id="naturalidade" value={naturalidade} onChange={e => setNaturalidade(e.target.value)}/>
                 </Box>
               </HStack>
               <HStack spacing="4">
                 <Box w="100%">
                   <FormLabel htmlFor="cel">Celular</FormLabel>
-                  <Input id="cel" type="number" />
+                  <Input id="celular" type="number" value={celular} onChange={e => setCelular(e.target.value)} />
                 </Box>
                 <Box w="100%">
                   <FormLabel htmlFor="Tefone">Telefone</FormLabel>
-                  <Input id="Tefone" type="number" />
+                  <Input id="telefone" type="number" value={telefone} onChange={e => setTelefone(e.target.value)} />
                 </Box>
               </HStack>
               <HStack spacing="4">
                 <Box w="100%">
                   <FormLabel htmlFor="endereco">Endereço</FormLabel>
-                  <Input id="endereco" />
+                  <Input id="endereco" value={endereco}  onChange={e => setEndereco(e.target.value)} />
                 </Box>
                 <Box w="100%">
                   <FormLabel htmlFor="cidade">Cidade</FormLabel>
-                  <Input id="cidade" />
+                  <Input id="cidade" value={cidade} onChange={e => setCidade(e.target.value)} />
                 </Box>
               </HStack>
               <HStack spacing="4">
@@ -116,7 +168,7 @@ export default function Formulario() {
                   Enviar
                 </Button>
               </HStack>
-            </FormControl>
+            </form>
           </Center>
         </Flex>
     </Box>
